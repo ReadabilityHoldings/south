314d0b5eb2b80c61e2c2333a4be36e1367d4af3f
Kevin O'Connor <kjoconnor@gmail.com>
2012-11-30 19:18:28 -0500

    Fixing the custom introspection on tagging

--- south/introspection_plugins/__init__.py
+++ south/introspection_plugins/__init__.py
@@ -3,7 +3,8 @@
 
 # These imports trigger the lower-down files
 import south.introspection_plugins.geodjango
-import south.introspection_plugins.django_tagging
+# This interferes with our own 'tagging' app
+#import south.introspection_plugins.django_tagging
 import south.introspection_plugins.django_taggit
 import south.introspection_plugins.django_objectpermissions
 import south.introspection_plugins.annoying_autoonetoone
